5a154187866e85ec045b62abcf68d82185c30138
duducesconetto <cesconettoedu@gmail.com>
2022-06-21 22:51:54 +0000

    start working ejs templates

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 const PORT = 8080; //default port
 
 const urlDatabase = {
