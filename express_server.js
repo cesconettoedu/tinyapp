e752d2ba3e042e1f16d9979ecdf15d0924b4aa12
duducesconetto <cesconettoedu@gmail.com>
2022-06-29 00:28:44 +0000

    login, register, edit and delete works

--- express_server.js
+++ express_server.js
@@ -1,21 +1,44 @@
 const express = require("express");
 const app = express();
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-//install another piece of middleware, body-parser
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
-
 const PORT = 8080; //default port
 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+app.set('view engine', 'ejs'); // set the view engine to ejs
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// Listener
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 app.listen(PORT, () => {
   console.log(`Express Server listening on port ${PORT}!`);
 });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// Database
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+const users = { }; // creating a user obj
+
+const err = {
+  '400': '400: Bad Request - Email or Password empty',
+  '401': '401: Unauthorized - Email already registered ',
+  '403': '403: Forbidden - Email or Password empty'
+};
+let errMsg = '';
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// Help Function
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //generating string of 6 random alphanumeric characters:
 let generateRandomString = () => {
@@ -28,50 +51,116 @@ let generateRandomString = () => {
   return result;
 };
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+//check if exist email before register
+const checkEmail = (email) => {
+  for (let obj in users) {
+    if (users[obj].email === email) {
+      return users[obj].id;
+    }
+  }
+  return undefined;
 };
 
-// add this to see if its redirect works
-app.get('/', (req, res) => {
-  res.status(301);
-  res.redirect('/urls');
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// Routes
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// create template to do registration
+app.get('/urls/register', (req, res) => {
+  const templateVars = {
+    username: users[req.cookies["user_id"]],
+  };
+  res.render("urls_register", templateVars);
 });
 
-// receive a username 
-app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('/urls');
+//add new user to Users
+app.post('/urls/register', (req, res) => {
+  if (req.body.email === '' || req.body.password === '') {  //if the box are unfiled
+    errMsg = err[400];
+    res.redirect('/urls/err');
+    //res.status(400).send('Bad Request');
+  } else if (checkEmail(req.body.email) !== undefined) {  //if find email already register
+    errMsg = err[401];
+    res.redirect('/urls/err');
+  }
+
+  let newUser = generateRandomString();
+  users[newUser] = {};
+  users[newUser].id = newUser;
+  users[newUser].email = req.body.email;
+  users[newUser].password = req.body.password;
+  console.log(users);  // show the actual and new user add
+  res.redirect('/urls/login');
+});
+
+// create template to do login
+app.get('/urls/login', (req, res) => {
+  const templateVars = {
+    username: users[req.cookies["user_id"]],
+    urls: urlDatabase
+  };
+  res.render("urls_login", templateVars);
+});
+
+// recieve and check if email is already register
+app.post('/urls/login', (req, res) => {
+  if (req.body.email === '' || req.body.password === '') {
+    errMsg = err[403];
+    res.redirect('/urls/err'); //........... em trabalho error 403
+  }                   //err[403]
+
+  let id = checkEmail(req.body.email);
+
+  if (id === undefined) {
+    res.redirect('/urls/register'); //mandar para o erro 403
+  } else if (users[id].password === req.body.password) {
+    res.cookie('user_id', id);
+    res.redirect('/urls');
+  } else {
+    res.redirect('/urls/login');
+  }
 });
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('username')
+  res.clearCookie('user_id');
   res.redirect('/urls');
 });
 
 //get the urls_index_template
 app.get('/urls', (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    username: users[req.cookies["user_id"]],
     urls: urlDatabase
   };
   res.render("urls_index", templateVars);
 });
 
+
+
+//adicionei para chamar os erros, ...................... em trabalho
+app.get("/urls/err", (req, res) => {
+  const templateVars = {
+    username: users[req.cookies["user_id"]],
+    err: errMsg
+  };
+  res.render("urls_err", templateVars);
+});
+
 //add get new routes needs to be before /urls/:id
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    username: users[req.cookies["user_id"]],
+    urls: urlDatabase
   };
   res.render("urls_new", templateVars);
 });
 
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { 
-    username: req.cookies["username"],
+  const templateVars = {
+    username: users[req.cookies["user_id"]],
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL] 
+    longURL: urlDatabase[req.params.shortURL]
   };
   res.render("urls_show", templateVars);
 });
@@ -98,10 +187,10 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 //add Edit to Myt URls and redirect to urls_show
 app.post("/urls/:shortURL/edit", (req, res) => {
-  const templateVars = { 
+  const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"]
+    username: users[req.cookies["user_id"]]
   };
   res.render("urls_show", templateVars);
 });
@@ -113,16 +202,5 @@ app.post("/urls/:shortURL/update", (req, res) => {
   res.redirect('/urls');  // redirect to MyUrls page
 });
 
-
-
-
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 
